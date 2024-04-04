9c8fab9bf446e64c7308370973a1b83e5f0c8e6f
NumMeril <misa.merkulov78@gmail.com>
2024-04-04 13:44:39 +0300

    begginig of adding masks

--- mandelbrot.c
+++ mandelbrot.c
@@ -8,6 +8,23 @@
 #include "TXLib.h"
 #include "config.h"
 
+//TODO
+inline void mm_set_int      ( int mm[4], int val )                                      ;
+inline int  mm_gz_int_val   ( int mm[4], int val )                                      ;
+inline void mm_sub_int_val  ( int mm[4], int val,          const int mask[4] )          ;
+inline void mm_add_int_val  ( int mm[4], int val,          const int mask[4] )          ;
+inline void mm_mul_int_val  ( int mm[4], int val,          const int mask[4] )          ;
+inline void mm_mul_int_mm2  ( int mm[4], const int mm2[4], const int mask[4] )          ;
+inline void mm_shr_int_val  ( int mm[4], int val )                                      ;
+
+inline void mm_set_fl       ( float mm[4], float val )                                  ;
+inline void mm_cpy_fl       ( float mm[4], const float mm2[4] )                         ;
+inline void mm_add_fl_val   ( float mm[4], float val,          const int mask[4] )      ;
+inline void mm_add_fl_mm2   ( float mm[4], const float mm2[4], const int mask[4] )      ;
+inline void mm_mul_fl_val   ( float mm[4], float val,          const int mask[4] )      ;
+inline void mm_mul_fl_mm2   ( float mm[4], const float mm2[4], const int mask[4] )      ;
+inline void mm_sub_fl_val   ( float mm[4], float val,          const int mask[4] )      ;
+inline void mm_sub_fl_mm2   ( float mm[4], const float mm2[4], const int mask[4] )      ;
 
 void HandleKeyStrokes( float* xC, float* yC, float* scale );
 
@@ -22,16 +39,17 @@ int main(){
     float yC = 0;       // Actual y coordinate of the left top corner of the window
     float scale  = 0.025;   // Shows, how much user zoomed in/out
 
-    int count       = 0;   // counter of iterations
-    int color       = 0;
-    float Mald_x    = 0;
-    float Mald_y    = 0;
-    float prev_x    = 0;
-    float prev_y    = 0;
-    float start_x   = 0;
-    float start_y   = 0;
+    int count[4]       = {};   // counter of iterations
+    int color[4]       = {};   // current color
+    bool mask[4]       = {};
+    float Mald_x[4]    = {};   // X position in Mandelbrot iterations
+    float Mald_y[4]    = {};   // Y position in Mandelbrot iterations
+    float prev_x[4]    = {};   // previous X position in Mandelbrot iterations
+    float prev_y[4]    = {};   // previous Y position in Mandelbrot iterations
+    float start_x[4]   = {};   // X position before Mandelbrot iterations
+    float start_y[4]   = {};   // X position before Mandelbrot iterations
 
-    float tmp = 0;
+    float tmp[4]       = {};
 
     for (;;) {
 
@@ -49,40 +67,71 @@ int main(){
 
         RGBQUAD* window = txVideoMemory();
         for ( int yPos = 0; yPos < WINDOW_HEIGHT; yPos++ ) {
-            for ( int xPos = 0; xPos < WINDOW_WIDTH; xPos++ ) {
+            for ( int xPos = 0; xPos < WINDOW_WIDTH; xPos+=4 ) {
 
-                color = START_COLOR;
-                count = MAXIMUM_ITERATIONS;
-                start_x = xC + xPos * scale;
-                start_y = yC + yPos * scale;
+                mm_set_int( color, DEFAULT_COLOR );
+                mm_set_int( count, MAXIMUM_ITERATIONS );
 
-                prev_x = start_x;
-                prev_y = start_y;
+                mm_set_fl( start_x, xC + xPos * scale );
+                mm_set_fl( start_y, yC + yPos * scale );
+
+                mm_cpy_fl( prev_x, start_x );
+                mm_cpy_fl( prev_y, start_y );
 
 
-                while (count--){
-                    Mald_x = prev_x * prev_x - prev_y * prev_y + start_x;
-                    Mald_y = 2 * prev_x * prev_y + start_y;
-                    color = color * color + START_COLOR;
+                while ( mm_gz_int_val( count, 0 ) ) {
+                    mm_cpy_fl    ( Mald_y, prev_x   );
+                    mm_mul_fl_mm2( Mald_y, prev_y   );
+                    mm_mul_fl_val( Mald_y, 2        );
+                    mm_add_fl_mm2( Mald_y, start_y  );
 
-                    tmp = Mald_x * Mald_x + Mald_y * Mald_y - start_y * start_y - start_x * start_x;
+                    mm_mul_fl_mm2( prev_x, prev_x   );
+                    mm_mul_fl_mm2( prev_y, prev_y   );
+                    mm_sub_fl_mm2( prev_x, prev_y   );
+                    mm_cpy_fl    ( Mald_x, prev_y   );
+                    mm_add_fl_mm2( Mald_x, start_x  );
 
-                    if ( tmp >  RADIUS * RADIUS || tmp < -RADIUS * RADIUS )
-                        break;
+                    mm_mul_int_mm2( color, color );
+                    mm_add_int_val( color, START_COLOR );
 
-                    prev_x = Mald_x;
-                    prev_y = Mald_y;
+                    mm_cpy_fl( prev_y, Mald_y );
+                    mm_mul_fl_mm2( prev_y, Mald_y );
+
+                    mm_cpy_fl( tmp, Mald_x );
+                    mm_mul_fl_mm2( tmp, Mald_x );
+
+                    mm_add_fl_mm2( tmp, prev_y );
+
+
+                    mm_cpy_fl( prev_x, start_x );
+                    mm_cpy_fl( prev_y, start_y );
+                    mm_mul_fl_mm2( prev_x, prev_x );
+                    mm_mul_fl_mm2( prev_y, prev_y );
+
+                    mm_sub_fl_mm2( tmp, prev_y );
+                    mm_sub_fl_mm2( tmp, prev_x );
+
+                    for ( int i = 0; i < 4; i++ )
+                        if ( tmp[i] > RADIUS * RADIUS || tmp[i] < -RADIUS * RADIUS )
+                            count[i] = -1;
+
+                    mm_cpy_fl( prev_x, Mald_x );
+                    mm_cpy_fl( prev_y, Mald_y );
+
+                    mm_sub_int_val( count, 1 );
 
                 }
 
-                if ( !count )
-                    color = DEFAULT_COLOR;
+                if ( count[0] == 0 || count[1] == 0 )
+                    mm_set_int( color, DEFAULT_COLOR );
 
-                window[xPos + (-yPos + WINDOW_HEIGHT - 1) * WINDOW_WIDTH].rgbBlue    = color % 256;
-                color >>= 8;
-                window[xPos + (-yPos + WINDOW_HEIGHT - 1) * WINDOW_WIDTH].rgbGreen   = color % 256;
-                color >>= 8;
-                window[xPos + (-yPos + WINDOW_HEIGHT - 1) * WINDOW_WIDTH].rgbRed     = color % 256;
+                for ( int i = 0; i < 4; i++ ){
+                    window[xPos + (-yPos + WINDOW_HEIGHT - 1) * WINDOW_WIDTH + i].rgbBlue    = color[i] % 256;
+                    mm_shr_int_val( color, 8 );
+                    window[xPos + (-yPos + WINDOW_HEIGHT - 1) * WINDOW_WIDTH + i].rgbGreen   = color[i] % 256;
+                    mm_shr_int_val( color, 8 );
+                    window[xPos + (-yPos + WINDOW_HEIGHT - 1) * WINDOW_WIDTH + i].rgbRed     = color[i] % 256;
+                }
             }
         }
 
@@ -100,6 +149,31 @@ int main(){
 }
 
 
+inline void mm_set_int      ( int mm[4], int val )                  { for ( int i = 0; i < 4; i++ ) mm[i] = val;        }
+
+inline int  mm_gz_int_val   ( int mm[4], int val )                  { int cnt = 0; for ( int i = 0; i < 4; i++ ) cnt += mm[i] > val; return cnt; }
+
+inline void mm_sub_int_val  ( int mm[4], int val, const int mask[4] )                  { for ( int i = 0; i < 4; i++ ) mm[i] -= val;       }
+
+inline void mm_add_int_val  ( int mm[4], int val )                  { for ( int i = 0; i < 4; i++ ) mm[i] += val;       }
+
+inline void mm_mul_int_val  ( int mm[4], int val )                  { for ( int i = 0; i < 4; i++ ) mm[i] *= val;       }
+inline void mm_mul_int_mm2  ( int mm[4], const int mm2[4] )         { for ( int i = 0; i < 4; i++ ) mm[i] *= mm2[i];    }
+
+inline void mm_shr_int_val  ( int mm[4], int val )                  { for ( int i = 0; i < 4; i++ ) mm[i] >>= val;      }
+
+
+inline void mm_set_fl       ( float mm[4], float val )              { for ( int i = 0; i < 4; i++ ) mm[i] = val;        }
+inline void mm_cpy_fl       ( float mm[4], const float mm2[4] )     { for ( int i = 0; i < 4; i++ ) mm[i] = mm2[i];     }
+
+inline void mm_add_fl_val   ( float mm[4], float val )              { for ( int i = 0; i < 4; i++ ) mm[i] += val;       }
+inline void mm_add_fl_mm2   ( float mm[4], const float mm2[4] )     { for ( int i = 0; i < 4; i++ ) mm[i] += mm2[i];    }
+
+inline void mm_mul_fl_val   ( float mm[4], float val )              { for ( int i = 0; i < 4; i++ ) mm[i] *= val;       }
+inline void mm_mul_fl_mm2   ( float mm[4], const float mm2[4] )     { for ( int i = 0; i < 4; i++ ) mm[i] *= mm2[i];    }
+
+inline void mm_sub_fl_val   ( float mm[4], float val )              { for ( int i = 0; i < 4; i++ ) mm[i] -= val;       }
+inline void mm_sub_fl_mm2   ( float mm[4], const float mm2[4] )     { for ( int i = 0; i < 4; i++ ) mm[i] -= mm2[i];    }
 
 
 void HandleKeyStrokes( float* xC, float* yC, float* scale ) {
